2f8542969d9638432ae83dea4916b54f114d2da5
CodeWingX <amresh.venugopal@gmail.com>
2016-03-29 11:53:25 +0530

    reduce page load time #1

--- home/scripts/phoneSwipeSim.js
+++ home/scripts/phoneSwipeSim.js
@@ -12,17 +12,14 @@ jQuery(function($){
       image1.css("display","block");
       image2.css("display","none");
       image3.css("display","none");
-      console.log("img1");
     } else if (timer === 1) {
       image1.css("display","none");
       image2.css("display","block");
       image3.css("display","none");
-      console.log("img2");
     } else if (timer === 2) {
       image1.css("display","none");
       image2.css("display","none");
       image3.css("display","block");
-      console.log("img3");
     }
   },3000);
 });
