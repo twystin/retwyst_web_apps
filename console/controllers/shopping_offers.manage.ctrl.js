ae4213939d2550d26559812576b69bf128503b8a
Kuldeep Patel <kuldeepp89@gmail.com>
2016-04-26 14:30:44 +0530

    some updates

---
+++ console/controllers/shopping_offers.manage.ctrl.js
@@ -0,0 +1,73 @@
+angular.module('consoleApp').controller('ShoppingOffersManageController', ['$scope', 'consoleRESTSvc', '$filter',
+	function($scope, consoleRESTSvc, $filter) {
+
+		$scope.searchKeywords = '';
+
+		$scope.shopping_offers = [];
+
+		$scope.filtered_shopping_offers = [];
+
+		$scope.row = '';
+
+		$scope.numPerPageOpt = [5, 10, 20, 40];
+
+		$scope.numPerPage = $scope.numPerPageOpt[1];
+
+		$scope.currentPage = 1;
+
+		$scope.current_page_shopping_offers = [];
+
+		consoleRESTSvc.getShoppingOffers().then(function(res) {
+			console.log(res);
+			$scope.shopping_offers = res.data;
+			$scope.search();
+			$scope.select($scope.currentPage);
+		}, function(err) {
+			console.log(err);
+		});
+
+		$scope.select = function(page) {
+			var start, end;
+			start = (page - 1) * $scope.numPerPage;
+			end = start + $scope.numPerPage;
+			return $scope.current_page_shopping_offers = $scope.filtered_shopping_offers.slice(start, end);
+		};
+
+		$scope.onFilterChange = function() {
+			$scope.select(1);
+			$scope.currentPage = 1;
+			return $scope.row = '';
+		};
+
+		$scope.onNumPerPageChange = function() {
+			$scope.select(1);
+			return $scope.currentPage = 1;
+		};
+
+		$scope.onOrderChange = function() {
+			$scope.select(1);
+			return $scope.currentPage = 1;
+		};
+
+		$scope.search = function() {
+			$scope.filtered_shopping_offers = $filter('filter')($scope.shopping_offers, $scope.searchKeywords);
+			if ($scope.view_status) {
+				$scope.sort($scope.view_status);
+			}
+			return $scope.onFilterChange();
+		};
+
+		$scope.order = function(rowName) {
+			if ($scope.row === rowName) {
+				return;
+			}
+
+			$scope.row = rowName;
+			$scope.filtered_shopping_offers = $filter('orderBy')($scope.shopping_offers, rowName);
+            if ($scope.view_status) {
+                $scope.sort($scope.view_status);
+            }
+            return $scope.onOrderChange();
+		};
+	}
+]);
