d557192def61de5e0f5db8fe1da51001f97a0f23
Kuldeep Patel <kuldeepp89@gmail.com>
2016-04-27 17:24:51 +0530

    some updates

--- console/controllers/cashback_coupon.create.ctrl.js
+++ console/controllers/cashback_coupon.create.ctrl.js
@@ -3,27 +3,8 @@ angular.module('consoleApp').controller('CashbackCouponCreateController', ['$sco
       $scope.searchKeywords = '';
       $scope.view_status = '';
       $scope.outlets = [];
-
-      console.log("SweetAlert: ", SweetAlert);
       $scope.filtered_outlets = [];
 
-      $scope.coupon = {
-        offers:[],
-        code:"#154652",
-        all:true,
-        only_on_first_order:true,
-        max_use_limit:15,
-        per_user_limit:"5",
-        start_date: new Date(2016, 3, 22),
-        end_date: new Date(2016, 3, 23),
-        actions:{
-          reward:{
-            reward_meta: {
-              reward_type:"flatoff",
-            }
-          }
-        }
-      };
 
         $scope.createCoupon = function() {
           console.log("coupon", $scope.coupon);
