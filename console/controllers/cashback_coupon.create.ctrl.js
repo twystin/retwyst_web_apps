d42b91fe0117687b1b37494fcb35374b3af4fa33
CodeWingX <amresh.venugopal@gmail.com>
2016-05-04 13:29:05 +0530

    Resolved: Routing after coupon creation, Coupon falsy validation

--- console/controllers/cashback_coupon.create.ctrl.js
+++ console/controllers/cashback_coupon.create.ctrl.js
@@ -1,12 +1,11 @@
-angular.module('consoleApp').controller('CashbackCouponCreateController', ['$scope', 'consoleRESTSvc', '$filter', '$modal', '$state', 'SweetAlert',
-    function($scope, consoleRESTSvc, $modal, $state, $filter, SweetAlert) {
+angular.module('consoleApp').controller('CashbackCouponCreateController', ['$scope', '$state', 'consoleRESTSvc', '$filter', '$modal', 'SweetAlert',
+    function($scope, $state, consoleRESTSvc, $modal, $filter, SweetAlert) {
       $scope.searchKeywords = '';
       $scope.view_status = '';
       $scope.outlets = [];
       $scope.filtered_outlets = [];
-
+      
         $scope.createCoupon = function() {
-          
           if (!_.get($scope.coupon, 'code')) {
             SweetAlert.swal('Validation Error', 'Coupon code not present', 'warning');
           } else if (!_.get($scope.coupon, 'max_use_limit')) {
@@ -17,9 +16,7 @@ angular.module('consoleApp').controller('CashbackCouponCreateController', ['$sco
             SweetAlert.swal('Validation Error', 'Start date not present', 'warning');
           } else if (!_.get($scope.coupon, 'end_date')) {
             SweetAlert.swal('Validation Error', 'End date not present', 'warning');
-          } else if (!$scope.coupon.actions) {
-            SweetAlert.swal('Validation Error', 'Please select reward type', 'warning');
-          }else if (!_.get($scope.coupon.actions.reward.reward_meta, 'reward_type')) {
+          } else if (!_.get($scope.coupon.actions.reward.reward_meta, 'reward_type')) {
             SweetAlert.swal('Validation Error', 'Reward type not present', 'warning');
           } else if (!_.get($scope.coupon.actions.reward.reward_meta, 'off') && $scope.coupon.actions.reward.reward_meta.reward_type === "flatoff" ) {
             SweetAlert.swal('Validation Error', 'Flat off amount not input', 'warning');
@@ -31,7 +28,7 @@ angular.module('consoleApp').controller('CashbackCouponCreateController', ['$sco
             SweetAlert.swal('Validation Error', 'Maximum discount amount not input', 'warning');
           } else {
             consoleRESTSvc.createCoupon($scope.coupon).then(function(res) {
-        			
+
         			SweetAlert.swal({
         				title: 'SUCCESS',
         				text: 'Coupon created successfully',
